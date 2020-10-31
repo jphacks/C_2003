61fc953f2f57c2d54435ab627eb1e9f6a3781b46
Masacccch <46284897+Masacccch@users.noreply.github.com>
2020-10-31 17:19:14 +0900

    Create MaTest & Set Log-utility

---
+++ JPHacks2020_c_2003/Assets/_Pien/Script/Save/BT1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BT1 : MonoBehaviour
+{
+
+    public Text boxtext ;
+    public GameObject text;
+    private List<string> strArray = null ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxtext = text.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // boxtext.text = "AAAAAAA";
+    }
+
+    public void Drop()
+    {
+        setStrArr(FileStr.getStringArr());
+        Debug.Log("Clicked!");
+        if(strArray != null)
+        {
+            foreach(var a in strArray)
+            {
+                boxtext.text += a+"\n";
+            }
+        }
+        // テキストボックスに文字入れる関数
+        
+        
+    }
+
+    public void setStrArr(List<string> arr)
+    {
+        strArray = arr;
+    }
+}
